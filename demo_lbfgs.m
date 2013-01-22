d81667c916fcc7f95893a882289f7ff6532035c9
David Larson <dplarson@ucsd.edu>
2013-01-21 17:21:57 -0800

    Add dLCL/db fcn for L-BFGS.

--- demo_lbfgs.m
+++ demo_lbfgs.m
@@ -7,10 +7,32 @@
 load('data/Web_normalized.mat');
 
 
+%% Preprocess data
+x = data(1:10,1:end-1);
+y = data(1:10,end);
+
+% initialize
+nFeatures = size(x,2);
+B0 = zeros(1,nFeatures); % parameters
+
+% function handle
+fhandle = @(B) derivativeLCL(x,y,B);
+
+%% Test function
+%meanDLCL = derivativeLCL(x, y, B0)
+
+
 %% Setup L-BFGS
+options.Method = 'lbfgs';
+options.Dispaly = 'iter';
+options.MaxFunEvals = 1E3;
+
+% add minFunc directory to path
+addpath('minFunc');
 
 
 %% Run L-BFGS
+x = minFunc(@(B)derivativeLCL(x,y,B),B0,options);
 
 
 %% Plot results
